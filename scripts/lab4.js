5a0f70b632fedf28616e2537fe776dd85a5766bd
yuankaiy <yuy080@ucsd.edu>
2025-04-28 23:09:45 -0700

    Fixed additional bugs in sumValues and discountPrices functions

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
@@ -27,6 +27,9 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
+    if(length == 0){
+        return false;
+    }
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
         discountedPrice = prices[i] * (1 - discount);
@@ -39,5 +42,5 @@ function discountPrices(prices, discount) {
 module.exports = {sumValues, discountPrices};
 
 // Tests
-console.log(sumValues(1,2,true));
-console.log(discountPrices([2,4], 0.5));
+// console.log(sumValues(1,2,false));
+// console.log(discountPrices([], 0.5));
